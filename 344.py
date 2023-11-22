6ad4c964649a8950bb982cbcc65f8eda21042584
LEEHYEYEONG <elto789@naver.com>
2023-11-22 14:16:54 +0900

    344 . Reverse String

---
+++ 344.py
@@ -0,0 +1,9 @@
+def reverseString(s):
+    s.reverse()
+
+# def reverseString(s):
+#     left, right = 0, len(s) - 1
+#     while left < right:
+#         s[left], s[right] = s[right], s[left]
+#         left += 1
+#         right -= 1
